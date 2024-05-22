b476cc409bbe873c0772a5f20306948617aea26d
esembera <ernest@bykonstrukt.com>
2024-05-22 21:45:25 +0200

    connect form to firebase

--- pphoto/app/schemas/formSchema.js
+++ pphoto/app/schemas/formSchema.js
@@ -1,6 +1,6 @@
 import { z } from "zod"
 export const formSchema = z.object({
-  selectedFile: z.instanceof(File, {message: "Photo to upload must be selected."}),
+  selectedFileUrl: z.string().optional(),
   photoName: z.string().min(2, "Photo name is required."),
   description: z.string().optional(),
   photoDate: z.date({message: "Date when the photo was taken is required."}),
--- pphoto/firebaseConfig.js
+++ pphoto/firebaseConfig.js
@@ -11,6 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -20,4 +21,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { auth, db, storage };
+export { db, storage };
