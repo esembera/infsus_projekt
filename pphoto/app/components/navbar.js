ed8dfcfc66067ebfddd274b52623c1e4008cff16
esembera <ernest@bykonstrukt.com>
2024-05-22 18:44:55 +0200

    add navigation menu and fix form error messages.

---
+++ pphoto/app/components/navbar.js
@@ -0,0 +1,36 @@
+"use client";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <div className="w-full border-2 rounded-sm">
+      <NavigationMenu className="ml-10">
+        <NavigationMenuList>
+          <NavigationMenuItem className="mr-5">
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Homepage
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/upload" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Upload picture
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  )
+}
+
+export default NavBar;
--- pphoto/app/schemas/formSchema.js
+++ pphoto/app/schemas/formSchema.js
@@ -1,12 +1,12 @@
 import { z } from "zod"
 export const formSchema = z.object({
-  selectedFile: z.instanceof(File),
-  photoName: z.string().min(2, "Photo name is required"),
+  selectedFile: z.instanceof(File, {message: "Photo to upload must be selected."}),
+  photoName: z.string().min(2, "Photo name is required."),
   description: z.string().optional(),
-  photoDate: z.date(),
+  photoDate: z.date({message: "Date when the photo was taken is required."}),
   tags: z.array(z.object({
     id: z.string(),
     text: z.string()
   })).optional(),
-  photoType: z.enum(["portrait", "landscape", "macro", "street", "screenshot"])
+  photoType: z.enum(["portrait", "landscape", "macro", "street", "screenshot"], {message: "Photo Type is required."})
 });
