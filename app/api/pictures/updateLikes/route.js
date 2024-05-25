14ae8af62d8203ebe2258e162e36b29b30034e1d
photovec <petar.hotovec44@gmail.com>
2024-05-25 17:26:42 +0200

    MVC architecture overhaul

--- app/api/pictures/updateLikes/route.js
+++ app/api/pictures/updateLikes/route.js
@@ -1,21 +1,5 @@
-import { NextResponse } from "next/server";
-import { db } from "@/firebaseConfig";
-import { doc, updateDoc } from "firebase/firestore";
+import { handleUpdateLikes } from "../../../controllers/picturesController";
 
 export async function POST(req) {
-  try {
-    const { id, likes } = await req.json();
-    const docRef = doc(db, "pictures", id);
-    await updateDoc(docRef, { likes });
-    return NextResponse.json(
-      { success: "Likes updated successfully!" },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error("Error updating likes:", error);
-    return NextResponse.json(
-      { error: "Error updating likes" },
-      { status: 500 }
-    );
-  }
+  return handleUpdateLikes(req);
 }
